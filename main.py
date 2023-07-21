626141ebf1e30f033b32a28f5ffacef1f111184a
Colton Fox <46362482+coltfox@users.noreply.github.com>
2023-07-21 13:57:36 -0400

    Fix incorrect TEST_LABELS_PATH

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from mnist_idx_reader import IDX1Reader, IDX3Reader
 from utils import display_number
 from config import Config
+import numpy as np
 
 CONFIG = Config(
     TRAINING_IMGS_PATH="./data/train-images.idx3-ubyte",
     TRAINING_LABELS_PATH="./data/train-labels.idx1-ubyte",
     TEST_IMGS_PATH="./data/t10k-images.idx3-ubyte",
-    TEST_LABELS_PATH="t10k-labels.idx1-ubyte",
+    TEST_LABELS_PATH="./data/t10k-labels.idx1-ubyte",
 )
 
 labels = IDX1Reader.read_from_file(CONFIG.TRAINING_LABELS_PATH)
